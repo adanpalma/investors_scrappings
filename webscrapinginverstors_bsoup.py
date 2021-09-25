7a871df2bc29aeb0a8fab199c54572b3f6609882
adan.palma <adanpalma@hotmail.com>
2021-09-25 16:45:10 -0500

    Se agrego un unpacking para los campos de  la base datos, se incluyo la clase date para ver si incorporar funcionalidad que solo muestre los reportes que no cumplen con la fecha de publicacion, es decir que la ultima fecha de publicacion no es igual a la fecha que se espera sea publicada.  Adicional se añadio un campo a la bd para que se incluya el formato que debera usarse con el metodo strftime de la clase date....

--- webscrapinginverstors_bsoup.py
+++ webscrapinginverstors_bsoup.py
@@ -1,51 +1,41 @@
 import sqlite3
-
+from datetime import date
 import requests as rq
 from bs4 import BeautifulSoup as bs
 
-# ssl.create_default_https_context allow download files
+#  allow download files from ssl
 # from a https protocol
 # ssl._create_default_https_context = ssl._create_unverified_context
 
-
+#TODO: incorparar date.strftime para ver si con el campo formato de fecha en
+# la base de datos se arma la estructura de la fecha en formato texto y se
+# compara para que la siguiente version del script solo muestre aquellos
+# reportes que su ultima publicacion no conincide con la que se espera
 try:
     # Abro conexion con sqllite3
     conn = sqlite3.connect("global_investror_publication_db.db")
     conn.row_factory = sqlite3.Row  # esto permite que los registros se obtengan
     # como un dictionary o hash
     cur = conn.cursor()
-    cur.execute(
-            'SELECT * from reports_parameters'
-            )
+    cur.execute('SELECT * from reports_parameters')
 
     main_url = "https://www.lahipotecaria.com"
     url_anterior = None
     for registro in cur.fetchall():
-        publication_name = registro['publication_name']
-        report_name = registro['report_name']
-        url = registro['url_link']
-        main_tag = registro['html_tag']
-        atr = {"class": registro['tag_attr']}
+        publication_name, report_name, url, main_tag, atr, publicated_month_tag, \
+        *others_fields = registro
 
-        # si viene null no se busca con find_all y se # busca por texto
-        publicated_month_tag = registro['publicated_month_tag']
-        publication_month_searched = "August, 2021"
-
-        # esto se hace porque hay urls iguales para no repetir los requests y
-        # mejorar un poco el performance
+        # como  hay urls iguales se controla que no se haga reques mas de 1 vez
         if url_anterior == None or url_anterior != url:
             htmlpage = rq.get(url)
             status_code = htmlpage.status_code
         url_anterior = url
 
-        # Si el link da error, el mismo se reporta y se continua con otros
-        # reportes
         if status_code != 200:
             print(
                     f""" {publication_name} 
                            {report_name} -> {url} status_code -> {status_code}
-                """
-                    )
+                """ )
             continue
 
         # TODO: convertir en un metodo reusable para todos los reportes
@@ -53,22 +43,19 @@ try:
 
         first_main_tags = bspage.find_all(main_tag, atr)
         for firsts_tags in first_main_tags:
-            "Si no hay month tag indica que esa pagina habra que buscar por texto"
-        if publicated_month_tag != None:
-            seconds_tag = firsts_tags.find(publicated_month_tag)
-            estado = seconds_tag.text.strip()
+            #  "Si no hay month tag indica que esa pagina habra que buscar por texto"
+            if publicated_month_tag != None:
+                seconds_tag = firsts_tags.find(publicated_month_tag)
+                estado = seconds_tag.text.strip()
+            else:
+                # al no tener publicated_month_tag se hace una busqueda por texto
+                estado = ""
+                for tags in first_main_tags:
+                    estado += tags.text.strip()
 
-        else:
-            # al no tener publicated_month_tag se hace una busqueda por texto
-            estado = ""
-            for tags in first_main_tags:
-                estado += tags.text.strip()
-
-        print(
-                f""" {publication_name} 
-{report_name} -> {publication_month_searched if 1 == 0 else ""}: {estado}
-               """
-                )
+            print(f""" {publication_name} 
+            {report_name} -> {publication_month_searched if 1 == 0 else ""}: {estado}
+                   """)
 
 finally:
     cur.close()
