17c7cce77b1cdbe2b4346b6fd0152e92f2df998e
adan.palma <adanpalma@hotmail.com>
2021-09-27 22:16:31 -0500

    Se agrego paquete csv para grabar la salida en formato csv... Adicional se le agrega la fecha  en la que  se corre el proceso y estoy viendo el tiempo que tarda el for de las busquedas que siento demora mucho

--- webscrapinginverstors_bsoup.py
+++ webscrapinginverstors_bsoup.py
@@ -1,5 +1,7 @@
 import sqlite3
+import csv
 from datetime import date
+import time
 import requests as rq
 from bs4 import BeautifulSoup as bs
 
@@ -7,23 +9,28 @@ from bs4 import BeautifulSoup as bs
 # from a https protocol
 # ssl._create_default_https_context = ssl._create_unverified_context
 
-#TODO: incorparar date.strftime para ver si con el campo formato de fecha en
+lineas_a_imprimir = []
+fecha_proceso = date.today()
+
+
+# TODO: incorparar date.strftime para ver si con el campo formato de fecha en
 # la base de datos se arma la estructura de la fecha en formato texto y se
 # compara para que la siguiente version del script solo muestre aquellos
 # reportes que su ultima publicacion no conincide con la que se espera
+
 try:
     # Abro conexion con sqllite3
     conn = sqlite3.connect("global_investror_publication_db.db")
     conn.row_factory = sqlite3.Row  # esto permite que los registros se obtengan
     # como un dictionary o hash
     cur = conn.cursor()
-    cur.execute('SELECT * from reports_parameters')
+    cur.execute('SELECT * from reports_parameters' )
 
     main_url = "https://www.lahipotecaria.com"
     url_anterior = None
     for registro in cur.fetchall():
         publication_name, report_name, url, main_tag, atr, publicated_month_tag, \
-        *others_fields = registro
+        *others_fields, salir_luego_primer_tag = registro
 
         # como  hay urls iguales se controla que no se haga reques mas de 1 vez
         if url_anterior == None or url_anterior != url:
@@ -35,13 +42,18 @@ try:
             print(
                     f""" {publication_name} 
                            {report_name} -> {url} status_code -> {status_code}
-                """ )
+                """
+                    )
             continue
 
         # TODO: convertir en un metodo reusable para todos los reportes
+
         bspage = bs(htmlpage.content, "html.parser")
 
+        #start_time = time.time()
         first_main_tags = bspage.find_all(main_tag, atr)
+        #print(f"Tiempo en Segundos para {publication_name} {report_name}
+        # {(time.time() - start_time)}")
         for firsts_tags in first_main_tags:
             #  "Si no hay month tag indica que esa pagina habra que buscar por texto"
             if publicated_month_tag != None:
@@ -53,9 +65,20 @@ try:
                 for tags in first_main_tags:
                     estado += tags.text.strip()
 
-            print(f""" {publication_name} 
-            {report_name} -> {publication_month_searched if 1 == 0 else ""}: {estado}
-                   """)
+            lineas_a_imprimir.append([publication_name, report_name,
+                    estado]
+                    )
+
+            if salir_luego_primer_tag == "S":
+                break
+
+    with open("International_Investments.csv", "w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([f"Fecha de Reporte {fecha_proceso.strftime('%B %d,%Y')}"])
+        writer.writerow(
+                ["Publication Name", "Report Name", "Last Publication Date"]
+                )
+        writer.writerows(lineas_a_imprimir)
 
 finally:
     cur.close()
